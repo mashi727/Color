2f16191c176d5076c8e9f2aedb8697e91007a08b
mashi <mashi.zzz@gmail.com>
2023-02-17 08:10:34 +0900

    1st commit

---
+++ imgPlotDockUi.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'imgPlotDockUi.ui'
+##
+## Created by: Qt User Interface Compiler version 6.4.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import (QApplication, QGridLayout, QHBoxLayout, QHeaderView,
+    QLabel, QMainWindow, QMenu, QMenuBar,
+    QPushButton, QSizePolicy, QSpacerItem, QSplitter,
+    QStatusBar, QTextBrowser, QTreeView, QVBoxLayout,
+    QWidget)
+
+from pyqtgraph.dockarea import DockArea
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1591, 1005)
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        self.actionOpen = QAction(MainWindow)
+        self.actionOpen.setObjectName(u"actionOpen")
+        self.actionQuit = QAction(MainWindow)
+        self.actionQuit.setObjectName(u"actionQuit")
+        self.actionAbout = QAction(MainWindow)
+        self.actionAbout.setObjectName(u"actionAbout")
+        self.actionOpenDialog = QAction(MainWindow)
+        self.actionOpenDialog.setObjectName(u"actionOpenDialog")
+        self.actionQuit2 = QAction(MainWindow)
+        self.actionQuit2.setObjectName(u"actionQuit2")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.splitter = QSplitter(self.centralwidget)
+        self.splitter.setObjectName(u"splitter")
+        self.splitter.setOrientation(Qt.Horizontal)
+        self.dockWidget = DockArea(self.splitter)
+        self.dockWidget.setObjectName(u"dockWidget")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.dockWidget.sizePolicy().hasHeightForWidth())
+        self.dockWidget.setSizePolicy(sizePolicy1)
+        self.splitter.addWidget(self.dockWidget)
+        self.widget = QWebEngineView(self.splitter)
+        self.widget.setObjectName(u"widget")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy2.setHorizontalStretch(1)
+        sizePolicy2.setVerticalStretch(1)
+        sizePolicy2.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+        self.widget.setSizePolicy(sizePolicy2)
+        self.widget.setMinimumSize(QSize(500, 500))
+        self.widget.setMaximumSize(QSize(16777215, 16777215))
+        self.splitter.addWidget(self.widget)
+
+        self.horizontalLayout.addWidget(self.splitter)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setMaximumSize(QSize(350, 16777215))
+
+        self.verticalLayout.addWidget(self.label_3)
+
+        self.treeView = QTreeView(self.centralwidget)
+        self.treeView.setObjectName(u"treeView")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Ignored, QSizePolicy.Expanding)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.treeView.sizePolicy().hasHeightForWidth())
+        self.treeView.setSizePolicy(sizePolicy3)
+        self.treeView.setMinimumSize(QSize(350, 458))
+        self.treeView.setMaximumSize(QSize(350, 16777215))
+        font = QFont()
+        font.setPointSize(10)
+        self.treeView.setFont(font)
+
+        self.verticalLayout.addWidget(self.treeView)
+
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setMaximumSize(QSize(350, 16777215))
+
+        self.verticalLayout.addWidget(self.label_2)
+
+        self.textBrowser = QTextBrowser(self.centralwidget)
+        self.textBrowser.setObjectName(u"textBrowser")
+        sizePolicy3.setHeightForWidth(self.textBrowser.sizePolicy().hasHeightForWidth())
+        self.textBrowser.setSizePolicy(sizePolicy3)
+        self.textBrowser.setMinimumSize(QSize(350, 0))
+        self.textBrowser.setMaximumSize(QSize(350, 200))
+
+        self.verticalLayout.addWidget(self.textBrowser)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+
+        self.horizontalLayout_4 = QHBoxLayout()
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.horizontalSpacer_2 = QSpacerItem(765, 17, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_4.addItem(self.horizontalSpacer_2)
+
+        self.quitButton = QPushButton(self.centralwidget)
+        self.quitButton.setObjectName(u"quitButton")
+        sizePolicy.setHeightForWidth(self.quitButton.sizePolicy().hasHeightForWidth())
+        self.quitButton.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout_4.addWidget(self.quitButton)
+
+
+        self.gridLayout.addLayout(self.horizontalLayout_4, 1, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 1591, 24))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menuFile.addAction(self.actionOpenDialog)
+        self.menuFile.addAction(self.actionQuit2)
+
+        self.retranslateUi(MainWindow)
+        self.quitButton.clicked.connect(MainWindow.close)
+        self.actionQuit2.triggered.connect(MainWindow.close)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
+        self.actionQuit.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
+        self.actionAbout.setText(QCoreApplication.translate("MainWindow", u"About", None))
+        self.actionOpenDialog.setText(QCoreApplication.translate("MainWindow", u"Open", None))
+        self.actionQuit2.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"File List", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Data List", None))
+        self.quitButton.setText(QCoreApplication.translate("MainWindow", u"Quit", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"File", None))
+    # retranslateUi
+
